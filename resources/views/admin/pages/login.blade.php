c2ed4500e784b6dcb3c4323cd535cf0b005c38fe
wherkamp <wherkamp@kingtux.me>
2018-09-28 16:51:00 -0400

    Found more admin things

--- resources/views/admin/pages/login.blade.php
+++ resources/views/admin/pages/login.blade.php
@@ -6,15 +6,15 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="/admin/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/assets/admin/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="/admin/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="/assets/admin/bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="/admin/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="/assets/admin/bower_components/Ionicons/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="/admin/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="/assets/admin/dist/css/AdminLTE.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="/admin/plugins/iCheck/square/blue.css">
+  <link rel="stylesheet" href="/assets/admin/plugins/iCheck/square/blue.css">
 
   <meta name="application-name" content="MinePoS">
   <meta name="author" content="AndrewAubury & PiggyPiglet">
@@ -26,7 +26,7 @@
   <meta property="og:url" content="{{url()->current()}}" />
   <meta property="og:title" content="MinePoS Admin" />
   <meta property="og:description" content="Login to access the administration panel of {{ env('APP_NAME') }}" />
-  <meta property="og:image" content="{{url('/admin/logo.png')}}" />
+  <meta property="og:image" content="{{url('/assets/admin/logo.png')}}" />
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
@@ -54,7 +54,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <div class="center"><img height="64" src="{{url('/admin/logo.png')}}"></div>
+    <div class="center"><img height="64" src="{{url('/assets/admin/logo.png')}}"></div>
     <a href="#">{{ env('APP_NAME') }} Admin Panel
   </div>
   <!-- /.login-logo -->
@@ -105,11 +105,11 @@
 <!-- /.login-box -->
 
 <!-- jQuery 3 -->
-<script src="/admin/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="/assets/admin/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="/admin/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="/assets/admin/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- iCheck -->
-<script src="/admin/plugins/iCheck/icheck.min.js"></script>
+<script src="/assets/admin/plugins/iCheck/icheck.min.js"></script>
 <script>
   $(function () {
     $('input').iCheck({
--- resources/views/admin/pages/passwordreset.blade.php
+++ resources/views/admin/pages/passwordreset.blade.php
@@ -6,15 +6,15 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="/admin/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/assets/admin/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="/admin/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="/assets/admin/bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="/admin/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="/assets/admin/bower_components/Ionicons/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="/admin/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="/assets/admin/dist/css/AdminLTE.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="/admin/plugins/iCheck/square/blue.css">
+  <link rel="stylesheet" href="/assets/admin/plugins/iCheck/square/blue.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -76,10 +76,10 @@
 <!-- /.login-box -->
 
 <!-- jQuery 3 -->
-<script src="/admin/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="/assets/admin/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="/admin/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="/assets/admin/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- iCheck -->
-<script src="/admin/plugins/iCheck/icheck.min.js"></script>
+<script src="/assets/admin/plugins/iCheck/icheck.min.js"></script>
 </body>
 </html>
