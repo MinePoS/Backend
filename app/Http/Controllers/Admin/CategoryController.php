52179cede7dd5cd901386ef77f489d8365733cc4
MinePoS Dedicated <no-reply@minepos.net>
2019-08-01 23:19:03 +0200

    Adding Code

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -26,6 +26,28 @@ class CategoryController extends Controller
 
     public function store(){
     	$this->authorize('create-categories');
+
+        $rules = ["name"=>"required","short_desc"=>"required","desc"=>'required'];
+
+        $validator = Validator::make(Input::all(), $rules);
+
+        if ($validator->fails())
+        {
+            return redirect()->back()->withErrors($validator);
+        }
+
+        $name = Request("name");
+        $sdesc = Request("short_desc");
+        $desc = Request("desc");
+
+        $c = new Category;
+        $c->name = $name;
+        $c->short_desc = $sdesc;
+        $c->desc = $desc;
+
+        $c->save();
+        return redirect()->route("admin.categories.index")->with(["good"=> "Server '$name' created"]);
+
     }
 
     public function edit(Category $category){
@@ -35,6 +57,28 @@ class CategoryController extends Controller
 
     public function update(Category $category){
 		$this->authorize('edit-categories');
+
+        $rules = ["name"=>"required","short_desc"=>"required","desc"=>'required'];
+
+        $validator = Validator::make(Input::all(), $rules);
+
+        if ($validator->fails())
+        {
+            return redirect()->back()->withErrors($validator);
+        }
+
+        $name = Request("name");
+        $sdesc = Request("short_desc");
+        $desc = Request("desc");
+
+        
+        $category->name = $name;
+        $category->short_desc = $sdesc;
+        $category->desc = $desc;
+
+        $category->save();
+        return redirect()->route("admin.categories.index")->with(["good"=> "Server '$name' edited"]);
+
     }
 
     public function delete(Category $category){
