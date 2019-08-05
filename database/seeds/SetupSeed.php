a02af7d981b4bef48ed53d67f4d1089ed9a03374
MinePoS Dedicated <no-reply@minepos.net>
2019-08-05 02:45:39 +0200

    Seed update

--- database/seeds/SetupSeed.php
+++ database/seeds/SetupSeed.php
@@ -18,22 +18,27 @@ class SetupSeed extends Seeder
     	//Admin::truncate();
 
         Permission::create(['name' => 'view-dashboard']);
-		Permission::create(['name' => 'view-profile']);
-		Permission::create(['name' => 'list-servers']);
-		Permission::create(['name' => 'delete-servers']);
-		Permission::create(['name' => 'edit-servers']);
-		Permission::create(['name' => 'create-servers']);
-		Permission::create(['name' => 'rekey-servers']);
-		Permission::create(['name' => 'list-categories']);
-		Permission::create(['name' => 'create-categories']);
-		Permission::create(['name' => 'edit-categories']);
-		Permission::create(['name' => 'delete-categories']);
-		Permission::create(['name' => 'list-players']);
-		Permission::create(['name' => 'view-players']);
-		Permission::create(['name' => 'ban-players']);
-		Permission::create(['name' => 'unban-players']);
-		Permission::create(['name' => 'view-players-bans']);
-		Permission::create(['name' => 'view-orders']);
+        Permission::create(['name' => 'view-profile']);
+        Permission::create(['name' => 'list-servers']);
+        Permission::create(['name' => 'delete-servers']);
+        Permission::create(['name' => 'edit-servers']);
+        Permission::create(['name' => 'create-servers']);
+        Permission::create(['name' => 'rekey-servers']);
+        Permission::create(['name' => 'list-categories']);
+        Permission::create(['name' => 'create-categories']);
+        Permission::create(['name' => 'edit-categories']);
+        Permission::create(['name' => 'delete-categories']);
+        Permission::create(['name' => 'list-players']);
+        Permission::create(['name' => 'view-players']);
+        Permission::create(['name' => 'ban-players']);
+        Permission::create(['name' => 'unban-players']);
+        Permission::create(['name' => 'view-players-bans']);
+        Permission::create(['name' => 'view-orders']);
+        Permission::create(['name' => 'list-products']);
+        Permission::create(['name' => 'create-products']);
+        Permission::create(['name' => 'edit-products']);
+        Permission::create(['name' => 'delete-products']);
+
 
 		$role = Role::create(['name' => 'full-access']);
         $role->givePermissionTo(Permission::all());
